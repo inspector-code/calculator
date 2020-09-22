be82f6aee71dd859e6040786aa4212b3b6ee5659
Artem Tsybulko <tsybulko.artem@gmail.com>
2020-09-23 00:38:20 +0300

    feat: completely new functionality

--- calculator/script.js
+++ calculator/script.js
@@ -1,74 +1,87 @@
 const digits = document.querySelectorAll('.digit')
-const operation = document.querySelectorAll('.operation')
+const operations = document.querySelectorAll('.operation')
 const display = document.getElementById('display')
 const controls = document.querySelectorAll('.control')
 const equal = document.querySelector('.equal')
 
-let currentNumber = ''
-let currentOperation = ''
-let res = 0
+let prevNum = ''
+let newNum = ''
+let result = ''
+let operatorType = ''
+let decimalUsed = false
 
 digits.forEach(item => item.addEventListener('click', e => {
-    currentNumber += e.target.value
-    display.value = currentNumber
+    const num = e.target.value
+    if (result) {
+        newNum = num
+        result = ''
+    } else {
+        if (num === '.') {
+            if (decimalUsed !== true) {
+                newNum += num
+                decimalUsed = true
+            }
+        } else {
+            newNum += num
+        }
+    }
+    display.value = newNum
 }))
 
-operation.forEach(item => item.addEventListener('click', e => {
-    switch (e.target.value) {
-        case '+':
-            currentOperation = '+'
-            res += parseFloat(currentNumber)
-            break
-        case '-':
-            currentOperation = '-'
-            if (res === 0) {
-                res = parseFloat(currentNumber)
-            } else {
-                res -= parseFloat(currentNumber)
-            }
-            break
-        case '*':
-            currentOperation = '*'
-            if (res === 0) {
-                res = parseFloat(currentNumber)
-            } else {
-                res *= parseFloat(currentNumber)
-            }
-            break
-        default:
-            console.log('что то пошло не так')
+operations.forEach(item => item.addEventListener('click', e => {
+    let operator = e.target.value
+    if (!result) {
+        prevNum = newNum
+    } else {
+        prevNum = result
     }
-    currentNumber = ''
-    display.value = res
+    newNum = ''
+    decimalUsed = false
+    operatorType = operator
+    result = ''
 }))
 
-equal.addEventListener('click', e => {
-    switch (currentOperation) {
+equal.addEventListener('click', () => {
+    decimalUsed = false
+    prevNum = parseFloat(prevNum)
+    newNum = parseFloat(newNum)
+
+    switch (operatorType) {
         case '+':
-            res += parseFloat(currentNumber)
+            result = prevNum + newNum
             break
         case '-':
-            res -= parseFloat(currentNumber)
+            result = prevNum - newNum
             break
         case '*':
-            res *= parseFloat(currentNumber)
+            result = prevNum * newNum
+            break
+        case '/':
+            result = prevNum / newNum
             break
         default:
-            console.log('что то пошло не так')
+            result = newNum
     }
-    currentNumber = '0'
-    display.value = res
+
+    prevNum = result
+    display.value = result
 })
 
-controls.forEach(item => item.addEventListener('click', e => {
-    if (e.target.value === 'ac') {
-        currentNumber = ''
-        currentOperation = ''
-        res = 0
-        display.value = '0'
-    } else if (e.target.value === 'sq') {
-        res = Math.pow(parseFloat(display.value), 2)
-        display.value = res
-        currentNumber = ''
+controls.forEach(item => item.addEventListener('click', () => {
+    switch (item.value) {
+        case 'ac':
+            prevNum = ''
+            newNum = ''
+            result = ''
+            operatorType = ''
+            decimalUsed = false
+            display.value = '0'
+            break
+        case 'del':
+            if (!result) {
+                newNum = newNum.slice(0, newNum.length - 1)
+                display.value = newNum
+            }
+            break
     }
 }))
