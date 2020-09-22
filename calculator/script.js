8893b6ddb031710cf9f745814385863d5851faf8
Artem Tsybulko <tsybulko.artem@gmail.com>
2020-09-22 17:54:53 +0300

    feat: add new functionality (+,-,*)

--- calculator/script.js
+++ calculator/script.js
@@ -1,17 +1,74 @@
-const digit = document.querySelectorAll('.digit')
+const digits = document.querySelectorAll('.digit')
 const operation = document.querySelectorAll('.operation')
 const display = document.getElementById('display')
+const controls = document.querySelectorAll('.control')
 const equal = document.querySelector('.equal')
 
 let currentNumber = ''
-let nextNumber = ''
+let currentOperation = ''
+let res = 0
 
-digit.forEach(item => item.addEventListener('click', (e) => {
+digits.forEach(item => item.addEventListener('click', e => {
     currentNumber += e.target.value
     display.value = currentNumber
 }))
 
-equal.addEventListener('click', (e) => {
-    display.value = eval(currentNumber)
+operation.forEach(item => item.addEventListener('click', e => {
+    switch (e.target.value) {
+        case '+':
+            currentOperation = '+'
+            res += parseFloat(currentNumber)
+            break
+        case '-':
+            currentOperation = '-'
+            if (res === 0) {
+                res = parseFloat(currentNumber)
+            } else {
+                res -= parseFloat(currentNumber)
+            }
+            break
+        case '*':
+            currentOperation = '*'
+            if (res === 0) {
+                res = parseFloat(currentNumber)
+            } else {
+                res *= parseFloat(currentNumber)
+            }
+            break
+        default:
+            console.log('что то пошло не так')
+    }
     currentNumber = ''
+    display.value = res
+}))
+
+equal.addEventListener('click', e => {
+    switch (currentOperation) {
+        case '+':
+            res += parseFloat(currentNumber)
+            break
+        case '-':
+            res -= parseFloat(currentNumber)
+            break
+        case '*':
+            res *= parseFloat(currentNumber)
+            break
+        default:
+            console.log('что то пошло не так')
+    }
+    currentNumber = '0'
+    display.value = res
 })
+
+controls.forEach(item => item.addEventListener('click', e => {
+    if (e.target.value === 'ac') {
+        currentNumber = ''
+        currentOperation = ''
+        res = 0
+        display.value = '0'
+    } else if (e.target.value === 'sq') {
+        res = Math.pow(parseFloat(display.value), 2)
+        display.value = res
+        currentNumber = ''
+    }
+}))
