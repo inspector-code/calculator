be05af13e0e4aa0c9f812b7c8a446c1dacd45cbc
Artem Tsybulko <tsybulko.artem@gmail.com>
2020-10-15 13:41:38 +0300

    fix: bug fix

--- calculator/script.js
+++ calculator/script.js
@@ -7,39 +7,12 @@ const homeButton = document.querySelector('.home1')
 const calculator = document.querySelector('.calculator')
 
 let prevNum = ''
-let newNum = 0
+let newNum = '0'
 let result = ''
 let operatorType = ''
 let decimalUsed = false
 
-digits.forEach(item => item.addEventListener('click', e => {
-    const num = e.target.dataset.digit
-    if (result) {
-        newNum = num
-        result = ''
-    } else {
-        if (num === '.') {
-            if (!decimalUsed) {
-                newNum += num
-                decimalUsed = true
-            }
-        } else {
-            newNum += num
-        }
-    }
-    display.value = parseFloat(newNum)
-}))
-
-operations.forEach(item => item.addEventListener('click', e => {
-    const operator = e.target.dataset.operation
-    !result ? prevNum = newNum : prevNum = result
-    newNum = ''
-    decimalUsed = false
-    operatorType = operator
-    result = 0
-}))
-
-equal.addEventListener('click', () => {
+const calcResult = () => {
     decimalUsed = false
     prevNum = parseFloat(prevNum)
     newNum = parseFloat(newNum)
@@ -64,30 +37,79 @@ equal.addEventListener('click', () => {
             result = newNum
     }
 
-    result = Math.round(result*1000000)/1000000;
+    result = Math.round(result * 1000000) / 1000000;
     prevNum = result
     display.value = result
-})
+}
+
+const reset = () => {
+    prevNum = ''
+    newNum = '0'
+    result = ''
+    operatorType = ''
+    decimalUsed = false
+}
+
+digits.forEach(item => item.addEventListener('click', e => {
+    const num = e.target.dataset.digit
+    if (result) {
+        newNum = num
+        result = ''
+        operatorType = ''
+    } else {
+        if (num === '.') {
+            if (!decimalUsed) {
+                newNum += num
+                decimalUsed = true
+            }
+        } else {
+            if (decimalUsed) {
+                newNum += num
+            } else {
+                newNum = String(parseFloat(newNum += num))
+            }
+        }
+    }
+    display.value = newNum
+}))
+
+equal.addEventListener('click', () => calcResult())
+
+operations.forEach(item => item.addEventListener('click', e => {
+    const operator = e.target.dataset.operation
+    !result && prevNum && newNum && calcResult()
+    !result ? prevNum = newNum : prevNum = result
+    newNum = ''
+    decimalUsed = false
+    operatorType = operator
+    result = 0
+}))
 
 controls.forEach(item => item.addEventListener('click', e => {
     switch (e.target.dataset.control) {
         case 'ac':
-            prevNum = ''
-            newNum = 0
-            result = ''
-            operatorType = ''
-            decimalUsed = false
+            reset()
             display.value = '0'
             break
         case 'del':
             if (!result) {
                 newNum = newNum.slice(0, newNum.length - 1)
-                newNum === '' ? newNum = '0' : display.value = parseFloat(newNum)
+                if (!newNum.length) {
+                    newNum = '0'
+                    display.value = '0'
+                } else {
+                    display.value = newNum
+                }
             }
             break
         case 'root':
-            result = Math.sqrt(result || prevNum ||  newNum)
-            display.value = result
+            if (+result < 0 || +prevNum < 0 || +newNum < 0) {
+                reset()
+                display.value = 'Oops, number < 0 :('
+            } else {
+                result = Math.sqrt(result || prevNum ||  newNum)
+                display.value = result
+            }
             break
         case 'minus':
             if (newNum && !result) {
